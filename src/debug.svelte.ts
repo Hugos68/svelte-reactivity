8d3d6061bff21269c06865db8785b4ab675feac8
hugos68 <hugokorteapple@gmail.com>
2024-03-04 13:21:00 +0100

    Stuffs

--- src/debug.svelte.ts
+++ src/debug.svelte.ts
@@ -1,6 +1,6 @@
 import { Ref } from './ref.svelte.js';
 
 export function debug<T extends Ref<unknown>[]>(...refs: T) {
-	const inspection = $inspect(refs);
+	const inspection = $inspect(refs.map((ref) => ref.value));
 	return inspection;
 }
