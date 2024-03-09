10a7debe1e0568c87c4fcf5ab5bd14e675cc77ee
hugos68 <hugokorteapple@gmail.com>
2024-03-09 14:36:35 +0100

    Added readonly

---
+++ .changeset/unlucky-taxis-argue.md
@@ -0,0 +1,5 @@
+---
+"svelte-reactivity": patch
+---
+
+Added readonly
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { ref, type Ref } from './ref.svelte.js';
+export { readonly, type Readonly } from './readonly.svelte.js';
 export { computed } from './computed.svelte.js';
 export { watch } from './watch.svelte.js';
